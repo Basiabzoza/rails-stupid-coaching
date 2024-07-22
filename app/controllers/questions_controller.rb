f61786b8053ed9120e5826608a1ede2527e6e2df
basia brzeziecka <basia.brzeziecka@gmail.com>
2024-07-22 21:52:53 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,20 @@
+
 class QuestionsController < ApplicationController
   def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I cannot hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "great!"
+    elsif @question.ends_with? ("?")
+      @answer = "Silly question, get dressed and go to work"
+    else
+      @answer = "I do not care, get dressed anf go to work!"
+
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   #get "url", to: "controller#action"
 
-  get "/ask", to: "questions#ask"
+  get "/ask", to: "questions#ask", as: :ask
+  get "/answer", to: "questions#answer", as: :answer
 end
